d06b8b1c0862a9863fe7a09857d5ef1df246a91c
mfsi-maneeshg <83696098+mfsi-maneeshg@users.noreply.github.com>
2021-05-05 17:36:06 +0530

    added the basic code of register page

---
+++ src/ListAndKey.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NumberList(){
+    const numberList = [1,2,3,4,5,6];
+    numberList.push(7);
+    const list = numberList.map((number)=>
+        <li key={number.toString()}>{number}</li>
+    );
+    return <ul>{list}</ul>
+}
+
+export default NumberList
